02b1630908458beb09531ab891e6093db5dccac5
RahMA79 <rahoma772004@gmail.com>
2024-08-27 21:49:13 +0300

    notes_item

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_app_bar.dart';
+import 'package:note_app/widgets/notes_item.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -15,6 +16,7 @@ class NotesView extends StatelessWidget {
               height: 50,
             ),
             CustomAppBar(),
+            NotesItem(),
           ],
         ),
       ),
---
+++ lib/widgets/notes_item.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.amber,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Text(
+                  'Build your app with Rahma Ashraf',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.black,
+                    size: 32,
+                  )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 12, right: 36),
+              child: Text(
+                'August 27, 2024',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
