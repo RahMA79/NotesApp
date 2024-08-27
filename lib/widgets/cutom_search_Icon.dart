0cecb1b3fd41e3465c8dcb184d0b819901589f21
RahMA79 <rahoma772004@gmail.com>
2024-08-27 21:23:15 +0300

    Notes_view AppBar

---
+++ lib/widgets/cutom_search_Icon.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CutomSearchIcon extends StatelessWidget {
+  const CutomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      width: 45,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withOpacity(0.07),
+      ),
+      child: Center(
+        child: Icon(
+          Icons.search,
+          size: 25,
+        ),
+      ),
+    );
+  }
+}
