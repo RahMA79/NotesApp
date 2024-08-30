526e718bdf19ab600af9d047f961a1ccb0107b82
RahMA79 <rahoma772004@gmail.com>
2024-08-30 14:17:03 +0300

    fetchNotes Cubit

---
+++ lib/cubits/FetchNotes/notes_cubit.dart
@@ -0,0 +1,15 @@
+import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:meta/meta.dart';
+import 'package:note_app/models/note_model.dart';
+
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+  List<NoteModel> notes = [];
+  fetchAllNotes() {
+    var notesBox = Hive.box<NoteModel>('NotesBox');
+    notes = notesBox.values.toList();
+  }
+}
